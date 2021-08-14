9270dd3752ddca4f43e2982e66082bf855675135
Minh Le <levan@DESKTOP-57OGPA2.(none)>
2021-08-14 11:21:09 +0700

    lol

--- commands/core/prefix.js
+++ commands/core/prefix.js
@@ -8,10 +8,14 @@ module.exports = {
     usage: '{prefix}prefix <new prefix>',
 
     execute(client, message, args) {
-        if (!args.length) return message.channel.send(`${client.emotes.error} - Please provide the new prefix!`);
+        if (message.member.hasPermission('MANAGE_SERVER')) {
+            if (!args.length) return message.channel.send(`${client.emotes.error} - Please provide the new prefix!`);
 
-        prefix.setPrefix(args.join(" "), message.guild.id);
+            prefix.setPrefix(args.join(" "), message.guild.id);
 
-        message.channel.send(`${client.emotes.success} - Successfully changed prefix to **${args[0]}**!`)
+            message.channel.send(`${client.emotes.success} - Successfully changed prefix to **${args[0]}**!`)
+        } else {
+            return message.channel.send(`${client.emotes.error} - Insufficient permission!`)
+        }
     }
 }
--- commands/utils/purge.js
+++ commands/utils/purge.js
@@ -6,17 +6,21 @@ module.exports = {
     usage: '{prefix}purge all/<amount>',
 
     execute(client, message, args) {
-        if (!args[0] || args[0].match(/all/gi)) {
-            message.channel.messages.fetch()
-                .then(messages => message.channel.bulkDelete(messages));
+        if (message.member.hasPermission('MANAGE_MESSAGES')) {
+            if (!args[0] || args[0].match(/all/gi)) {
+                message.channel.messages.fetch()
+                    .then(messages => message.channel.bulkDelete(messages));
 
-            message.channel.send(`${client.emotes.success} - Successfully purged all messages!`)
+                message.channel.send(`${client.emotes.success} - Successfully purged all messages!`)
+            } else {
+                let messageCount = parseInt(args[0]);
+                message.channel.messages.fetch({ limit: messageCount })
+                    .then(messages => message.channel.bulkDelete(messages));
+
+                message.channel.send(`${client.emotes.success} - Successfully purged **${args[0]}** messages!`)
+            }
         } else {
-            let messageCount = parseInt(args[0]);
-            message.channel.messages.fetch({ limit: messageCount })
-                .then(messages => message.channel.bulkDelete(messages));
-
-            message.channel.send(`${client.emotes.success} - Successfully purged **${args[0]}** messages!`)
+            return message.channel.send(`${client.emotes.error} - Insufficient permission!`);
         }
     }
 }
