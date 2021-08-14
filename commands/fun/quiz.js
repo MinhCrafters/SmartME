a7c08d987a5e8e9aaf0ea023ae5a02d16f73f70f
Minh Le <levan@DESKTOP-57OGPA2.(none)>
2021-08-15 00:07:25 +0700

    lol

--- commands/fun/quiz.js
+++ commands/fun/quiz.js
@@ -7,18 +7,29 @@ module.exports = {
 	aliases: [],
 	description: 'Asks you some questions.',
 	category: 'Fun',
-	usage: '{prefix}quiz [difficulty]',
+	usage: '{prefix}quiz [difficulty] [type]',
 
 	async execute(client, message, args) {
 
         let difficultyLevels = ['easy', 'medium', 'hard'];
+        let questionTypes = ['multiple', 'boolean'];
 
         if (!args[0]) {
             difficultyLevel = functions.choice(difficultyLevels);
         } else {
             difficultyLevel = args[0];
         }
-		const response = await fetch(`https://opentdb.com/api.php?amount=20&difficulty=${difficultyLevel}`);
+
+        if (!args[1]) {
+            questionType = functions.choice(questionTypes);
+        } else {
+            questionType = args[1];
+            if (questionType === 'trueorfalse') {
+                questionType = 'boolean';
+            }
+        }
+
+		const response = await fetch(`https://opentdb.com/api.php?amount=20&difficulty=${difficultyLevel}&type=${questionType}`);
 		const data = await response.json();
 		var length = data.results.length;
 
