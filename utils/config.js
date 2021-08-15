969fcebf34ee704ced078bf13befeac0d5043215
MinhCrafters <59521026+MinhCrafters@users.noreply.github.com>
2021-08-15 12:17:34 +0700

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -10,9 +10,9 @@ module.exports = {
 
     discord: {
         token: process.env.TOKEN,
-        prefix: 'me!',
+        prefix: 'prefix here',
         activityType: 'PLAYING',
-        activity: '{prefix}help',
+        activity: 'activity here',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'bassboost_low', 'bassboost_high', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
